f4040c44abe4824a908a54c8f6e56c9c10ab3c3a
kagekyaa <kevin_hokagero@yahoo.com>
2015-11-21 13:41:57 -0800

    oxford3

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -6,28 +6,29 @@ from datetime import datetime
 from flask import render_template
 from FlaskWebProject import app
 import pyoxford
-from tokens import *
+# from tokens import *
 
 @app.route('/')
 @app.route('/home')
 def home():
     """Renders the home page."""
-    text = "a b c d e f"
-    api = pyoxford.speech("kage-test-speech", oxford_computer_speech)
-
-    # text to speech (.wav file)
-    binary = api.text_to_speech(text)
-    with open("sound1.wav", "wb") as f:
-        f.write(binary)
-
-    # speech to text
-    recognized = api.speech_to_text("sound1.wav")
-    print(recognized)
-
-    if text == recognized:
-        print(recognized)
-        print("success!!")
-
+    # bing_token = "bHBDnK+h8L79Mrmp8M0PHfyogYuTrpd6PM25bBh4S9A"
+    # oxford_computer_speech = "d6814acbebb940cd8553e0b125cc63a1"
+    # text = "a b c d e f"
+    # api = pyoxford.speech("kage-test-speech", oxford_computer_speech)
+    #
+    # # text to speech (.wav file)
+    # binary = api.text_to_speech(text)
+    # with open("sound1.wav", "wb") as f:
+    #     f.write(binary)
+    #
+    # # speech to text
+    # recognized = api.speech_to_text("sound1.wav")
+    # print(recognized)
+    #
+    # if text == recognized:
+    #     print(recognized)
+    #     print("success!!")
 
     return render_template(
         'index.html',
