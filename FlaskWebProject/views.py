0b4700d01be4bedb935c0d8b0646fdf7323736e1
kagekyaa <kevin_hokagero@yahoo.com>
2015-11-21 15:27:10 -0800

    testoxford4

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -6,22 +6,22 @@ from datetime import datetime
 from flask import render_template
 from FlaskWebProject import app
 import pyoxford
-# from tokens import *
+from tokens import *
 
 @app.route('/')
 @app.route('/home')
 def home():
     """Renders the home page."""
-    # text = "a b c d e f"
-    # api = pyoxford.speech("kage-test-speech", oxford_computer_speech)
-    #
-    # # text to speech (.wav file)
-    # binary = api.text_to_speech(text)
+    text = "a b c d e f"
+    api = pyoxford.speech("kage-test-speech", oxford_computer_speech)
+
+    # text to speech (.wav file)
+    binary = api.text_to_speech(text)
     # with open("sound1.wav", "wb") as f:
     #     f.write(binary)
     #
     # # speech to text
-    # recognized = api.speech_to_text("sound1.wav")
+    recognized = api.speech_to_text("sound1.wav")
     # print(recognized)
     #
     # if text == recognized:
