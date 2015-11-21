b1fb180782e1fefbc8c0b1f2dc15f541556e475b
kagekyaa <kevin_hokagero@yahoo.com>
2015-11-21 15:59:00 -0800

    oxfordtest10

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -1,8 +1,10 @@
 """
 The flask application package.
 """
+import os
+tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__, template_folder=tmpl_dir)
 
 import FlaskWebProject.views
