17f962f9e7c11695c51f5b36537eb368bf3e65e8
kagekyaa <kevin_hokagero@yahoo.com>
2015-11-21 13:32:11 -0800

    adding oxford2

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -12,24 +12,26 @@ from tokens import *
 @app.route('/home')
 def home():
     """Renders the home page."""
+    text = "a b c d e f"
+    api = pyoxford.speech("kage-test-speech", oxford_computer_speech)
+
+    # text to speech (.wav file)
+    binary = api.text_to_speech(text)
+    with open("sound1.wav", "wb") as f:
+        f.write(binary)
+
+    # speech to text
+    recognized = api.speech_to_text("sound1.wav")
+    print(recognized)
+
+    if text == recognized:
+        print(recognized)
+        print("success!!")
+
+
     return render_template(
         'index.html',
         title='Parrot me',
         year=datetime.now().year,
     )
 
-text = "a b c d e f"
-api = pyoxford.speech("kage-test-speech", oxford_computer_speech)
-
-# text to speech (.wav file)
-binary = api.text_to_speech(text)
-with open("sound1.wav", "wb") as f:
-    f.write(binary)
-
-# speech to text
-recognized = api.speech_to_text("sound1.wav")
-print(recognized)
-
-if text == recognized:
-    print(recognized)
-    print("success!!")
