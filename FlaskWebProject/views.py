ec790c9a08df6f88b4bbda1c3ae187e1d6832c90
kagekyaa <kevin_hokagero@yahoo.com>
2015-11-21 17:38:11 -0800

    oxfordtest17

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -5,6 +5,7 @@ Routes and views for the flask application.
 from datetime import datetime
 from flask import render_template
 from FlaskWebProject import app
+import http.client, urllib.parse, json
 import pyoxford
 # from tokens import *
 
@@ -23,26 +24,57 @@ def gameon():
     bing_token = "bHBDnK+h8L79Mrmp8M0PHfyogYuTrpd6PM25bBh4S9A"
     oxford_computer_speech = "d6814acbebb940cd8553e0b125cc63a1"
 
-    text = "a b c d e f"
-    api = pyoxford.speech("kage-test-speech", oxford_computer_speech)
+    clientId = "kage-test-speech"
+    clientSecret = oxford_computer_speech
+    ttsHost = "https://speech.platform.bing.com"
 
-    # text to speech (.wav file)
-    # binary = api.text_to_speech(text)
-    # with open('/sound1.wav', 'wb') as f:
-    #      f.write(binary)
+    params = urllib.parse.urlencode({'grant_type': 'client_credentials', 'client_id': clientId, 'client_secret': clientSecret, 'scope': ttsHost})
 
-    # speech to text
-    # recognized = api.speech_to_text("sound1.wav")
-    # print(recognized)
+    # print ("The body data: %s" %(params))
 
-    # if text == recognized:
-    #     print(recognized)
-    #     print("success!!")
+    headers = {"Content-type": "application/x-www-form-urlencoded"}
+
+    AccessTokenHost = "oxford-speech.cloudapp.net"
+    path = "/token/issueToken"
+
+    # Connect to server to get the Oxford Access Token
+    conn = http.client.HTTPSConnection(AccessTokenHost)
+    conn.request("POST", path, params, headers)
+    response = conn.getresponse()
+    # print(response.status, response.reason)
+
+    data = response.read()
+    conn.close()
+    accesstoken = data.decode("UTF-8")
+    # print ("Oxford Access Token: " + accesstoken)
+
+    #decode the object from json
+    ddata=json.loads(accesstoken)
+    access_token = ddata['access_token']
+
+    # Read the binary from wave file
+    f = open('sound1.wav','rb')
+    try:
+        body = f.read()
+    finally:
+        f.close()
+
+    headers = {"Content-type": "audio/wav; samplerate=8000",
+                "Authorization": "Bearer " + access_token}
+
+    #Connect to server to recognize the wave binary
+    conn = http.client.HTTPSConnection("speech.platform.bing.com")
+    conn.request("POST", "/recognize/query?scenarios=ulm&appid=D4D52672-91D7-4C74-8AD8-42B1D98141A5&locale=en-US&device.os=wp7&version=3.0&format=xml&requestid=1d4b6030-9099-11e0-91e4-0800200c9a66&instanceid=1d4b6030-9099-11e0-91e4-0800200c9a66", body, headers)
+    response = conn.getresponse()
+    # print(response.status, response.reason)
+    data = response.read()
+    # print(data)
+    conn.close()
 
     return render_template(
         'game.html',
         title = 'Parrot Me',
-        word = recognized
+        word = data
     )
 
 @app.route('/signup')
