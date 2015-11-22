1eb5478c5c4e6d1700ee5c7bea30913eaef69e5d
kagekyaa <kevin_hokagero@yahoo.com>
2015-11-21 18:02:44 -0800

    oxfordtest23

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -26,11 +26,26 @@ def gameon():
     a = 1
     b = 2
     data = a + b
+    text = "a b c eye"
+    api = pyoxford.speech("kage-test-speech", oxford_computer_speech)
+
+    # text to speech (.wav file)
+    binary = api.text_to_speech(text)
+    with open("sound1.wav", "wb") as f:
+        f.write(binary)
+
+    # speech to text
+    recognized = api.speech_to_text("sound1.wav")
+    # print(recognized)
+    #
+    # if text == recognized:
+    #     print(recognized)
+    #     print("success!!")
 
     return render_template(
         'game.html',
         title = 'Parrot Me',
-        word = data
+        word = recognized
     )
 
 @app.route('/signup')
