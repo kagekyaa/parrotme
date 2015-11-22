378bc4d8be78dc6d13a93d83cff669af5dc586a4
kagekyaa <kevin_hokagero@yahoo.com>
2015-11-22 01:07:34 -0800

    ioerror7

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -67,7 +67,7 @@ def game():
     #     finally:
     #         f.close()
 
-        w = wave.open("/static/music/sound1.wav", "rb")
+        w = wave.open("FlaskWebProject/static/music/sound1.wav", "rb")
         body = w.readframes(w.getnframes())
         w.close()
 
