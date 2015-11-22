baa3ffbee37aa6f9ee3ee0a8cb397d6c4ded414c
kagekyaa <kevin_hokagero@yahoo.com>
2015-11-21 16:02:21 -0800

    oxfordtest11

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -27,9 +27,9 @@ def gameon():
     api = pyoxford.speech("kage-test-speech", oxford_computer_speech)
 
     # text to speech (.wav file)
-    binary = api.text_to_speech(text)
-    with open("sound1.wav", "wb") as f:
-        f.write(binary)
+    # binary = api.text_to_speech(text)
+    # with open("sound1.wav", "wb") as f:
+    #     f.write(binary)
 
     # speech to text
     recognized = api.speech_to_text("sound1.wav")
