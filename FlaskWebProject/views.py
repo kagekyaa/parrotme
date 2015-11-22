10557baa4e92acd25f090d502ed7ad15f1bda1fd
kagekyaa <kevin_hokagero@yahoo.com>
2015-11-21 17:41:44 -0800

    oxfordtest18

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -24,52 +24,9 @@ def gameon():
     bing_token = "bHBDnK+h8L79Mrmp8M0PHfyogYuTrpd6PM25bBh4S9A"
     oxford_computer_speech = "d6814acbebb940cd8553e0b125cc63a1"
 
-    clientId = "kage-test-speech"
-    clientSecret = oxford_computer_speech
-    ttsHost = "https://speech.platform.bing.com"
-
-    params = urllib.parse.urlencode({'grant_type': 'client_credentials', 'client_id': clientId, 'client_secret': clientSecret, 'scope': ttsHost})
-
-    # print ("The body data: %s" %(params))
-
-    headers = {"Content-type": "application/x-www-form-urlencoded"}
-
-    AccessTokenHost = "oxford-speech.cloudapp.net"
-    path = "/token/issueToken"
-
-    # Connect to server to get the Oxford Access Token
-    conn = http.client.HTTPSConnection(AccessTokenHost)
-    conn.request("POST", path, params, headers)
-    response = conn.getresponse()
-    # print(response.status, response.reason)
-
-    data = response.read()
-    conn.close()
-    accesstoken = data.decode("UTF-8")
-    # print ("Oxford Access Token: " + accesstoken)
-
-    #decode the object from json
-    ddata=json.loads(accesstoken)
-    access_token = ddata['access_token']
-
-    # Read the binary from wave file
-    f = open('sound1.wav','rb')
-    try:
-        body = f.read()
-    finally:
-        f.close()
-
-    headers = {"Content-type": "audio/wav; samplerate=8000",
-                "Authorization": "Bearer " + access_token}
-
-    #Connect to server to recognize the wave binary
-    conn = http.client.HTTPSConnection("speech.platform.bing.com")
-    conn.request("POST", "/recognize/query?scenarios=ulm&appid=D4D52672-91D7-4C74-8AD8-42B1D98141A5&locale=en-US&device.os=wp7&version=3.0&format=xml&requestid=1d4b6030-9099-11e0-91e4-0800200c9a66&instanceid=1d4b6030-9099-11e0-91e4-0800200c9a66", body, headers)
-    response = conn.getresponse()
-    # print(response.status, response.reason)
-    data = response.read()
-    # print(data)
-    conn.close()
+    a = 1
+    b = 2
+    data = a + b
 
     return render_template(
         'game.html',
